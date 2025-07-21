f19c1b6dfa9a97f5b4aebf073527d4ad705237d9
USERJUL20 <USERJUL20@DESKTOP-8L0D19J>
2025-07-20 19:53:50 -0500

    Deber-Semana8

---
+++ Semana8/src/main/java/com/mycompany/semana8/Semana8.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semana8;
+
+/**
+ *
+ * @author USERJUL20
+ */
+public class Semana8 {
+
+    public static void main(String[] args) {
+       Dashboard dashboard = new Dashboard();
+        dashboard.iniciar();
+    }
+    }
